762a26246374999245c468263ab05ae497197f51
harouna227 <issifiharouna07@gmail.com>
2024-08-04 18:41:23 +0200

    control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -11,6 +12,39 @@ from kivy.uix.widget import Widget
 class MainWidget(Widget):
     pass
 
+
+class WidgetExemple(GridLayout):
+    compteur = 1
+    compteur_actif = BooleanProperty(False)
+    mon_text = StringProperty('1') 
+    text_input_str = StringProperty('Geek')
+    # slide_value_txt = StringProperty('50')
+    def on_button_click(self):
+        print("Button click")
+        if  self.compteur_actif:
+            self.compteur += 1
+            self.mon_text = str(self.compteur)
+        
+    def on_toggle_button_state(self, widget):
+        # print('Toggle state: ' + widget.state)
+        if widget.state == 'normal':
+            print('OFF')
+            widget.text = 'OFF'
+            self.compteur_actif = False
+        else:
+            print('ON')   
+            widget.text = 'ON' 
+            self.compteur_actif = True    
+    
+    def on_switch_active(self, Widget):
+        print('Switch: ' + str(Widget.active))
+        
+    # def on_slider_value(self, widget):
+        # print('Slider value: ' + str(int(widget.value)))
+        # self.slide_value_txt = str(int(widget.value))
+    
+    def on_text_validate(self, widget):
+        self.text_input_str = widget.text
 
 
 class StackLayoutExemple(StackLayout):
